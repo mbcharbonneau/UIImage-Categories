1261ba67be2cfa574b081742aec39b0ea907bc37
Nelson Tai <chiahsien@gmail.com>
2013-09-23 09:35:55 +0800

    Improved solution for a colorspace / transparency issue that affects some types of images.

--- UIImage+Resize.m
+++ UIImage+Resize.m
@@ -31,7 +31,7 @@
     UIImage *resizedImage = [self resizedImageWithContentMode:UIViewContentModeScaleAspectFill
                                                        bounds:CGSizeMake(thumbnailSize, thumbnailSize)
                                          interpolationQuality:quality];
-    
+
 
     // Crop out any part of the image that's larger than the thumbnail size
     // The cropped rect must be centered on the resized image
@@ -41,9 +41,9 @@
                                  thumbnailSize,
                                  thumbnailSize);
     UIImage *croppedImage = [resizedImage croppedImage:cropRect];
-    
+
     UIImage *transparentBorderImage = borderSize ? [croppedImage transparentBorderImage:borderSize] : croppedImage;
-    
+
     return [transparentBorderImage roundedCornerImage:cornerRadius borderSize:borderSize];
 }
 
@@ -62,9 +62,9 @@
         default:
 	    drawTransposed = NO;
     }
-        
+
     CGAffineTransform transform = [self transformForOrientation:newSize];
-    
+
     return [self resizedImage:newSize transform:transform drawTransposed:drawTransposed interpolationQuality:quality];
 }
 
@@ -75,22 +75,22 @@
     CGFloat horizontalRatio = bounds.width / self.size.width;
     CGFloat verticalRatio = bounds.height / self.size.height;
     CGFloat ratio;
-    
+
     switch (contentMode) {
         case UIViewContentModeScaleAspectFill:
             ratio = MAX(horizontalRatio, verticalRatio);
             break;
-            
+
         case UIViewContentModeScaleAspectFit:
             ratio = MIN(horizontalRatio, verticalRatio);
             break;
-            
+
         default:
             [NSException raise:NSInvalidArgumentException format:@"Unsupported content mode: %d", contentMode];
     }
-    
+
     CGSize newSize = CGSizeMake(self.size.width * ratio, self.size.height * ratio);
-    
+
     return [self resizedImage:newSize interpolationQuality:quality];
 }
 
@@ -108,59 +108,74 @@
     CGRect newRect = CGRectIntegral(CGRectMake(0, 0, newSize.width*scale, newSize.height*scale));
     CGRect transposedRect = CGRectMake(0, 0, newRect.size.height, newRect.size.width);
     CGImageRef imageRef = self.CGImage;
-    
-    // Fix for a colorspace / transparency issue that affects some types of 
+
+    // Fix for a colorspace / transparency issue that affects some types of
     // images. See here: http://vocaro.com/trevor/blog/2009/10/12/resize-a-uiimage-the-right-way/comment-page-2/#comment-39951
-        
-	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    size_t bitsPerComponent = CGImageGetBitsPerComponent(imageRef);
+    size_t bytesPerRow = 0; // CGImageGetBytesPerRow() calculates incorrectly in iOS 5.0, so defer to CGBitmapContextCreate()
+    CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(imageRef);
+
+    if (CGColorSpaceGetNumberOfComponents(colorSpace) == 3) {
+        int alpha = (bitmapInfo & kCGBitmapAlphaInfoMask);
+        if (alpha == kCGImageAlphaNone) {
+            bitmapInfo &= ~kCGBitmapAlphaInfoMask;
+            bitmapInfo |= kCGImageAlphaNoneSkipFirst;
+        } else if (!(alpha == kCGImageAlphaNoneSkipFirst || alpha == kCGImageAlphaNoneSkipLast)) {
+            bitmapInfo &= ~kCGBitmapAlphaInfoMask;
+            bitmapInfo |= kCGImageAlphaPremultipliedFirst;
+        }
+    }
+
     CGContextRef bitmap = CGBitmapContextCreate(
                                                 NULL,
                                                 newRect.size.width,
                                                 newRect.size.height,
-                                                8, /* bits per channel */
-                                                (newRect.size.width * 4), /* 4 channels per pixel * numPixels/row */
+                                                bitsPerComponent
+                                                bytesPerRow
                                                 colorSpace,
-                                                kCGImageAlphaPremultipliedLast
+                                                bitmapInfo
                                                 );
     CGColorSpaceRelease(colorSpace);
-	
+
     // Rotate and/or flip the image if required by its orientation
     CGContextConcatCTM(bitmap, transform);
-    
+
     // Set the quality level to use when rescaling
     CGContextSetInterpolationQuality(bitmap, quality);
-    
+
     // Draw into the context; this scales the image
     CGContextDrawImage(bitmap, transpose ? transposedRect : newRect, imageRef);
-    
+
     // Get the resized image from the context and a UIImage
     CGImageRef newImageRef = CGBitmapContextCreateImage(bitmap);
     UIImage *newImage = [UIImage imageWithCGImage:newImageRef scale:self.scale orientation:UIImageOrientationUp];
-    
+
     // Clean up
     CGContextRelease(bitmap);
     CGImageRelease(newImageRef);
-    
+
     return newImage;
 }
 
 // Returns an affine transform that takes into account the image orientation when drawing a scaled image
 - (CGAffineTransform)transformForOrientation:(CGSize)newSize {
     CGAffineTransform transform = CGAffineTransformIdentity;
-    
+
     switch (self.imageOrientation) {
         case UIImageOrientationDown:           // EXIF = 3
         case UIImageOrientationDownMirrored:   // EXIF = 4
             transform = CGAffineTransformTranslate(transform, newSize.width, newSize.height);
             transform = CGAffineTransformRotate(transform, M_PI);
             break;
-            
+
         case UIImageOrientationLeft:           // EXIF = 6
         case UIImageOrientationLeftMirrored:   // EXIF = 5
             transform = CGAffineTransformTranslate(transform, newSize.width, 0);
             transform = CGAffineTransformRotate(transform, M_PI_2);
             break;
-            
+
         case UIImageOrientationRight:          // EXIF = 8
         case UIImageOrientationRightMirrored:  // EXIF = 7
             transform = CGAffineTransformTranslate(transform, 0, newSize.height);
@@ -169,14 +184,14 @@
         default:
             break;
     }
-    
+
     switch (self.imageOrientation) {
         case UIImageOrientationUpMirrored:     // EXIF = 2
         case UIImageOrientationDownMirrored:   // EXIF = 4
             transform = CGAffineTransformTranslate(transform, newSize.width, 0);
             transform = CGAffineTransformScale(transform, -1, 1);
             break;
-            
+
         case UIImageOrientationLeftMirrored:   // EXIF = 5
         case UIImageOrientationRightMirrored:  // EXIF = 7
             transform = CGAffineTransformTranslate(transform, newSize.height, 0);
@@ -185,7 +200,7 @@
         default:
             break;
     }
-    
+
     return transform;
 }
 
