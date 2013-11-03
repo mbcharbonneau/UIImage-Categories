01009a0a8efe81f693f0be8304a729bae3e5f808
chenly <chenly@gmail.com>
2013-11-03 20:38:47 +0800

    kCGImageAlphaPremultipliedLast Fix
    Changes the type of the last argument to a call to CGBitmapContextCreate
    (without changing the actual value passed in the argument) to silence
    a compiler warning in Xcode 5.

--- UIImage+Resize.m
+++ UIImage+Resize.m
@@ -120,7 +120,7 @@
                                                 8, /* bits per channel */
                                                 (newRect.size.width * 4), /* 4 channels per pixel * numPixels/row */
                                                 colorSpace,
-                                                kCGImageAlphaPremultipliedLast
+                                                kCGBitmapAlphaInfoMask & kCGImageAlphaPremultipliedLast
                                                 );
     CGColorSpaceRelease(colorSpace);
 	
