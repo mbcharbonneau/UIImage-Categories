e704cfb4bf16ac5e046bea23a4f4b680af08458f
Brian Smith <bws@hypnotoad.local>
2013-11-04 10:22:16 +0100

    Added resizedImageWithMaximumDimension method : Resizes the given input IF either dimension exceeds the maximum, but keeps the aspect ratio.  Great for making thumbnails which don’t exceed a certain width or height.

--- UIImage+Resize.h
+++ UIImage+Resize.h
@@ -5,19 +5,29 @@
 
 // Extends the UIImage class to support resizing/cropping
 @interface UIImage (Resize)
+
 - (UIImage *)croppedImage:(CGRect)bounds;
+
 - (UIImage *)thumbnailImage:(NSInteger)thumbnailSize
           transparentBorder:(NSUInteger)borderSize
                cornerRadius:(NSUInteger)cornerRadius
        interpolationQuality:(CGInterpolationQuality)quality;
+
 - (UIImage *)resizedImage:(CGSize)newSize
      interpolationQuality:(CGInterpolationQuality)quality;
+
+- (UIImage *)resizedImageWithMaximumDimension:(CGFloat)maximumWidthOrHeight
+     interpolationQuality:(CGInterpolationQuality)quality;
+
 - (UIImage *)resizedImageWithContentMode:(UIViewContentMode)contentMode
                                   bounds:(CGSize)bounds
                     interpolationQuality:(CGInterpolationQuality)quality;
+
 - (UIImage *)resizedImage:(CGSize)newSize
                 transform:(CGAffineTransform)transform
            drawTransposed:(BOOL)transpose
      interpolationQuality:(CGInterpolationQuality)quality;
+
 - (CGAffineTransform)transformForOrientation:(CGSize)newSize;
+
 @end
