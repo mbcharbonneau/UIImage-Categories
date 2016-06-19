d2c664f51fa7f69db750cfa4542126ba63fcf1e1
Iryna Vasylieva <iyuna9119@gmail.com>
2016-06-19 23:34:31 +0300

    Created project with shared build scheme for compatibility with Carthage.

---
+++ UIImageCategories.h
@@ -0,0 +1,12 @@
+//
+//  UIImage-Categories.h
+//  UIImage-Categories
+//
+//  Created by Iyuna on 6/19/16.
+//  Copyright © 2016 UIImage-Categories. All rights reserved.
+//
+
+#import <UIImageCategories/UIImage+Alpha.h>
+#import <UIImageCategories/UIImage+Resize.h>
+#import <UIImageCategories/UIImage+RoundedCorner.h>
+
